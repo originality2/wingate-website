ac6cc2d34dcf2efd95531ff39d81587a62ce33fe
nich1jb <nich2jb@gmail.com>
2026-09-22 14:17:26 +1000

    fix things

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/wingate-website/",
   plugins: [react()],
   test: {
     globals: true,
